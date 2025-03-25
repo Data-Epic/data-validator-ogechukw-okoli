b1d1e0e295e07478d531a097ce6b56bccd96cb6b
Human-Gechi <kimberlydaniel363@gmail.com>
2025-03-25 05:57:33 +0100

    changes to package name and description of my package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-16") as file:
+    long_description = file.read()
 setup(
-    name='datavalidate',
+    name='datacheckerpro',
     version='0.1',
     packages=find_packages(),
     install_requires=[],
@@ -12,7 +12,7 @@ setup(
     description="A simple datavalidator for validating email, phone numbers, dates, and URLs.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="https://github.com/Data-Epic/data-validator-ogechukwu-okoli",
+    url="https://github.com/Data-Epic/data-validator-ogechukw-okoli",
     classifiers=[
         'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
